c8a4549614619719a67caec44569d879a4fd16ef
AvivS9 <avivshlomi@gmail.com>
2024-07-25 22:58:34 +0300

    init

--- run.sh
+++ run.sh
@@ -1,2 +1,16 @@
 
-run main.py
+#!/bin/bash
+echo 'hello world'
+
+
+# git clone <X>
+
+# cd <X>
+
+# docker build -t <Y> .
+
+# time = $(date)
+
+# docker run <Y> > output.txt # Expose the ports
+
+# echo "Build completed at $time" > output.txt
