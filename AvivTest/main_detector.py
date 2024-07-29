99ce7820445942526ff0d648b75a2ca09c08d485
AvivS9 <avivshlomi@gmail.com>
2024-07-30 01:26:32 +0300

    detector logic

--- AvivTest/main_detector.py
+++ AvivTest/main_detector.py
@@ -1,42 +1,37 @@
 import requests
 import json
+import detector_logic
 
 # API endpoint URL
-baseUrl = "https://funny-buses-agree.loca.lt"
+baseUrl = "https://little-rabbits-move.loca.lt"
 
 authenticationToken_for_detector = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWFtSWQiOiIyIiwidGVhbU5hbWUiOiJBdml2RGV0ZWN0b3IxIiwiaWF0IjoxNzIyMjgzNjIwLCJleHAiOjE3MjIzNzAwMjB9.rD6FOrD-h22_1l5CQQqSY7pMOj0qZCOeFaXyy48Ev1A'
 
 # Request body
 payload = {
-    "num_of_users": 11,
+    "num_of_users": 4
 }
 
-headers = {'Authorization': 'bearer ' + authenticationToken_for_detector }
+headers = {'Authorization': 'bearer ' + authenticationToken_for_detector, 'Content-Type': 'application/json' }
 
 sessionId = 1
 
 try:
 
     # ask for Session Info
-
-    response = requests.get(baseUrl + '/api/detector/session/1', headers=headers, data=payload) 
-    print('asdf' + str(response))
-
-    # do some logic
-
-    # Detect Session
-
+    sessionResponse = requests.get(baseUrl + '/api/detector/session/' + str(sessionId), headers=headers) 
     
-
-
+    detections = detector_logic.calculateDetections(sessionResponse.json())
+    print('detections ' + str(detections))
+    detection = requests.post(baseUrl + '/api/detector/session/' + str(sessionId), headers=headers, data=detections) 
 
     
     # Check if the request was successful
-    response.raise_for_status()
+    detection.raise_for_status()
     
     # Print the response
-    print("Response status code:", response.status_code)
-    print("Response content:", response.json())
+    print("Response status code:", detection.status_code)
+    print("Response content:", detection.json())
 
 except requests.exceptions.RequestException as e:
     print("An error occurred:", e)
--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 
 #!/bin/bash
-echo 'Running Aviv Test'
-cd AvivTest
-python3 main_detector.py
+python3 AvivTest/main_detector.py
