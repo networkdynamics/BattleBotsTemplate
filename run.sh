2f3126dbef1f73579fd190a9773b02b7bb776ec1
AvivS9 <avivshlomi@gmail.com>
2024-08-26 20:12:29 +0300

    add install pydantic

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Set base_url and authentication_token from command line arguments
-export BASE_URL=$1  
+# export BASE_URL=$1  
 #http://localhost:3000
-export AUTH_TOKEN=$2
+# export AUTH_TOKEN=$2
 #BOT: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWFtSWQiOiIxIiwidGVhbU5hbWUiOiJCb3QxIiwiaWF0IjoxNzI0NjQ0MTQzLCJleHAiOjE3MjQ3MzA1NDN9.09K-DBNfF7fKFmFwifsu2fi5hSX4iRogbYaZQMEMJKg
 #DETECTOR: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWFtSWQiOiIzIiwidGVhbU5hbWUiOiJEZXRlY3RvcjEiLCJpYXQiOjE3MjQ0NDIxMTYsImV4cCI6MTcyNDUyODUxNn0.-qXvRUh8Qm0zq-hveZOni2nKs-onzQ7ubPyQF-Aji3M
 
